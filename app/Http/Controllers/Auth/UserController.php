0ce674b140e4b148d0b39ece1c5a0dee9420f79e
iambasanta <sangpangbasanta@gmail.com>
2021-11-19 20:45:30 +0545

    implement user authentication with sanctum tokens

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $attributes = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:7'
+        ]);
+
+        User::create($attributes);
+
+        return response('Registered Successfully.',Response::HTTP_OK);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response([
+            'Logged in Successfully.',
+            'access_token'=>$token
+        ],Response::HTTP_OK);
+    }
+
+    public function profile(){
+        return response([
+            'User Profile.',
+            'data'=> auth()->user()
+        ],Response::HTTP_OK);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return response('Logged out Successfully.',Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('user/register',[UserController::class,'register']);
+Route::post('user/login',[UserController::class,'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware'=>'auth:sanctum'],function(){
+    Route::get('user/profile',[UserController::class,'profile']);
+    Route::get('user/logout',[UserController::class,'logout']);
 });
