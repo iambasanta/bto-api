68919f179b4a0fe05efe6b941d52bc8eaa2fcb2a
iambasanta <sangpangbasanta@gmail.com>
2021-11-29 18:56:59 +0545

    (feat): send ticket purchased notification to user via email

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Payment;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PaymentResource;
 use App\Models\Event;
+use App\Notifications\TicketPurchasedNotification;
 use App\Providers\TicketPayment;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,11 +17,14 @@ class PaymentController extends Controller
         return PaymentResource::collection($payments);
     }
     public function payment(Request $request,Event $event){
+        $user = auth()->user();
         $amount = $event->ticket_price * 100;
 
-        $payment = auth()->user()->charge($amount,$request->paymentMethod['id']);
+        $payment = $user->charge($amount,$request->paymentMethod['id']);
 
         event(new TicketPayment($event, $request->ticket_count, $amount, $payment->id));
+
+        $user->notify(new TicketPurchasedNotification);
 
         return response('Payment Successfull !',Response::HTTP_OK);
     }
